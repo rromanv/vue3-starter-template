3e6686313aab3718130ba22ddce6840800ee0ac6
Rodolfo Román <rromanv@users.noreply.github.com>
2021-04-15 03:29:33 -0500

    Added purge to tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors')
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
