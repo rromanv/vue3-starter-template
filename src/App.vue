9ffd3305f8d5c65b45931884cab60812bfa52c62
Rodolfo Roman <rromanv@gmail.com>
2021-04-20 22:31:13 -0500

    Changed class order

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="min-h-screen flex justify-center items-center text-coolGray-300 font-extralight space-x-4 text-6xl"
+    class="flex items-center justify-center min-h-screen space-x-4 text-6xl text-coolGray-300 font-extralight"
   >
     <mdi:controller-classic-outline />
     <h1 class="">Hello There</h1>
