2051160ad9acd88d28e55466164f67076708f803
Rodolfo Roman <rromanv@gmail.com>
2021-04-20 23:24:56 -0500

    Moved styles for better layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <div
-    class="flex items-center justify-center min-h-screen space-x-4 text-6xl text-coolGray-300 font-extralight"
-  >
+  <div class="main">
     <mdi:controller-classic-outline />
     <h1 class="">Hello There</h1>
   </div>
 </template>
+
+<style lang="postcss">
+  .main {
+    @apply flex items-center justify-center min-h-screen space-x-4 text-6xl text-coolGray-300 font-extralight;
+  }
+</style>
